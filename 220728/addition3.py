ef752735e0fcc5b1135374fd5e3ab81f64d21472
ferrorist <ferrorist@naver.com>
2022-07-28 16:46:09 +0900

    220728/addition3.py 주석 추가

--- 220728/addition3.py
+++ 220728/addition3.py
@@ -6,6 +6,10 @@ def solution(left, right):
     for i in range(left, right+1):
         count = 0
         for j in range(1, int(math.sqrt(i))+1):
+          # 짝수의 경우 제곱근을 제외하면 서로 짝을 이룬다.
+          # ex) 12일 경우, (1, 12), (2, 6), (3, 4)
+          # 1,2,3의 경우 12의 제곱근보다 작으므로,
+          # 제곱근보다 작거나 같은 정수만 탐색을 진행하면 된다. 
            if i%j == 0:
               if j**2 == i:
                 count += 1
@@ -19,4 +23,5 @@ def solution(left, right):
 
 left = int(input())
 right = int(input())
+# left, right = map(int, input().split())
 print(solution(left, right))
