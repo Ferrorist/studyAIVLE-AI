50ad12a7afb7d416910d3a0a9c46067925613131
ferrorist <ferrorist@naver.com>
2022-07-28 10:05:33 +0900

    change maximum-3.py

--- maximum-3.py
+++ maximum-3.py
@@ -1,3 +1,7 @@
+def max3(a, b, c):
+    maximum = a if (a > b) else b if (b > c) else c
+    return maximum
+
 print('정수의 최대값을 구합니다.')
 a = int(input('정수 a의 값을 입력하시오. :'))
 b = int(input('정수 b의 값을 입력하시오. :'))
@@ -9,5 +13,5 @@ c = int(input('정수 c의 값을 입력하시오. :'))
 # if c > maximum:
 #     maximum = c
 
-maximum = a if (a > b) else b if (b > c) else c
-print(f'최대값은 {maximum}입니다.')
+#maximum = a if (a > b) else b if (b > c) else c
+print(f'최대값은 {max3(a,b,c)}입니다.')
