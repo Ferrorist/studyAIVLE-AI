ed038b095c6dd91d3fe6d947dd9e902b341570a9
ferrorist <ferrorist@naver.com>
2022-07-28 13:50:08 +0900

    amend practice1.py v1.0.2

--- 220728/practice1.py
+++ 220728/practice1.py
@@ -1,17 +1,17 @@
 def electricPay(n):
     primary = 410
-    cost = 0;
+    cost = 0
     if n < 100:
         cost = 60.7*n;
     else:
-        cost += 60.7*100;
+        cost += 60.7*100
         if n < 200:
             primary = 910
             cost += 125.9*(n-100)
         else:
             cost += 125.9*100
             primary = 1600
-            cost += 187.9*(n-200);
+            cost += 187.9*(n-200)
     return primary+cost
 
 electric = int(input('사용 전력량을 입력하세요.: '))
