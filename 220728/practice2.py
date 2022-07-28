1b66914441cfa24f70f215e3dfcadb6b2a9f4881
ferrorist <ferrorist@naver.com>
2022-07-28 14:07:58 +0900

    amend practice2.py v1.0.1

--- 220728/practice2.py
+++ 220728/practice2.py
@@ -1,10 +1,8 @@
 # 실습 02
 count = int(input('+와 -를 번갈아 출력합니다.\n몇 개를 출력할까요? '))
-plus = True
-for _ in range(count):
-    if plus:
+for i in range(count):
+    if i%2==0:
         print('+', end='')
     else:
         print('-', end='')
-    plus = False if plus else True
 print()
