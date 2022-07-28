caf50a10f2e6be626748a5e8da8cf79202ecc68e
ferrorist <ferrorist@naver.com>
2022-07-28 17:07:37 +0900

    주석 추가

--- 220728/addition5.py
+++ 220728/addition5.py
@@ -1,14 +1,14 @@
 # 추가 실습 05
 # https://www.acmicpc.net/problem/18406
 def lucky(n):
-    score = str(n)
-    mid = int(len(score)/2)
+    score = str(n) # 연산의 편의성을 위해서 문자열로 형변환.
+    mid = int(len(score)/2) # 점수의 자릿수의 중간을 기준으로 계산하니까.
     left = 0
     right = 0
     for i in range(mid):
-        left += int(score[i])
+        left += int(score[i]) # 좌측 숫자 합
     for i in range(mid, len(score)):
-        right += int(score[i])
+        right += int(score[i]) # 우측 숫자 합
     
     if left == right:
         return "LUCKY"
--- 220728/addition6.py
+++ 220728/addition6.py
@@ -2,8 +2,8 @@
 # https://programmers.co.kr/learn/courses/30/lessons/12934
 import math
 def solution(n):
-    x = int(math.sqrt(n))
-    if n == x**2:
+    x = int(math.sqrt(n)) # x를 n의 제곱근의 정수부분으로 변경.
+    if n == x**2: # x가 정수라면 x**2 = n이 나와야 한다.
         return (x+1)**2
     else:
         return -1
