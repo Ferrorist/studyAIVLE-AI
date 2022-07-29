721666a387d526e59a669fc6b480df29069aed42
ferrorist <ferrorist@naver.com>
2022-07-29 15:18:49 +0900

    주석 추가 addition4.py

--- 220729/addition4.py
+++ 220729/addition4.py
@@ -1,11 +1,12 @@
 # 실습 문제 04
 # https://programmers.co.kr/learn/courses/30/lessons/12953
-def gcd(a, b):
+# 유클리드 호제법 활용
+def gcd(a, b): # 최대공약수
     if b > a:
         a, b = b, a
     return gcd(b, a%b) if a%b else b
 
-def lcm(a, b):
+def lcm(a, b): # 최소공배수
     return abs(a*b)/gcd(a,b)
 
 
@@ -15,6 +16,6 @@ def solution(arr):
         answer = lcm(answer, num)
     return answer
 
-arr = [2,6,8,14]
-answer = solution(arr)
-print(answer)
+# arr = [2,6,8,14]
+# answer = solution(arr)
+# print(answer)
